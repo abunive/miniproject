b3f72de1b33f9b68f9f969f827e4d8c2b9ad8b2a
AswathiAnandhakumar <aswathishoba887@gmail.com>
2026-03-30 23:37:47 +0530

    fixed router issue

--- src/App.jsx
+++ src/App.jsx
@@ -62,12 +62,12 @@
 
 // export default App;
 import { Routes, Route } from "react-router-dom";
-// import AdminDashboard from "./AdminDashboard";
-// import StudentDashboard from "./StudentDashboard";
-// import FacultyDashboard from "./FacultyDashboard";
-// import OrganizerDashboard from "./OrganizerDashboard";
-// import FacultyNotifications from "./FacultyNotifications";
-// import StudentNotifications from "./StudentNotifications";
+import AdminDashboard from "./AdminDashboard";
+import StudentDashboard from "./StudentDashboard";
+import FacultyDashboard from "./FacultyDashboard";
+import OrganizerDashboard from "./OrganizerDashboard";
+import FacultyNotifications from "./FacultyNotifications";
+import StudentNotifications from "./StudentNotifications";
 import UserLogin from "./UserLogin";
 import AdminLogin from "./AdminLogin";
 import "./login.css";
@@ -79,12 +79,12 @@ function App() {
         <Route path="/admin-login" element={<AdminLogin />} />
         <Route path="/user-login" element={<UserLogin />} />
 
-        {/* <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
         <Route path="/student-dashboard" element={<StudentDashboard />} />
         <Route path="/faculty-dashboard" element={<FacultyDashboard />} />
         <Route path="/organizer-dashboard" element={<OrganizerDashboard />} />
         <Route path="/faculty-notifications" element={<FacultyNotifications />} />
-        <Route path="/student-notifications" element={<StudentNotifications />} /> */}
+        <Route path="/student-notifications" element={<StudentNotifications />} />
       </Routes>
   );
 }
