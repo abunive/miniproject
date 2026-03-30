f8d4c154a31abd5abf5d9ca5c3fb1c64626dc9ac
AswathiAnandhakumar <aswathishoba887@gmail.com>
2026-03-30 23:14:00 +0530

    fixed router issue

--- src/App.jsx
+++ src/App.jsx
@@ -28,8 +28,40 @@
 // }
 
 // export default App;             
-import { Routes, Route } from "react-router-dom";
-// import LoginSelector from "./LoginSelector";
+// import { BrowserRouter,Routes, Route } from "react-router-dom";
+// // import LoginSelector from "./LoginSelector";
+// import AdminDashboard from "./AdminDashboard";
+// import StudentDashboard from "./StudentDashboard";
+// import FacultyDashboard from "./FacultyDashboard";
+// import OrganizerDashboard from "./OrganizerDashboard";
+// import FacultyNotifications from "./FacultyNotifications";
+// import StudentNotifications from "./StudentNotifications";
+// import UserLogin from "./UserLogin";
+// // import Layout from "./Layout";
+// import "./login.css";
+// import AdminLogin from "./AdminLogin";
+
+// function App() {
+//   return (
+//     <Routes>
+//        <Route path="/admin-login" element={<AdminLogin />} />
+//       <Route path="/user-login" element={<UserLogin />} />
+//       {/* All logged-in users */}
+//       {/* <Route element={<Layout />}> */}
+//         <Route path="/admin-dashboard" element={<AdminDashboard />} />
+//         <Route path="/student-dashboard" element={<StudentDashboard />} />
+//         <Route path="/faculty-dashboard" element={<FacultyDashboard />} />
+//         <Route path="/organizer-dashboard" element={<OrganizerDashboard />} />
+//         <Route path="/faculty-notifications" element={<FacultyNotifications />} />
+// <Route path="/student-notifications" element={<StudentNotifications />} />
+//       {/* </Route> */}
+//     </Routes>
+    
+//   );
+// }
+
+// export default App;
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AdminDashboard from "./AdminDashboard";
 import StudentDashboard from "./StudentDashboard";
 import FacultyDashboard from "./FacultyDashboard";
@@ -37,25 +69,24 @@ import OrganizerDashboard from "./OrganizerDashboard";
 import FacultyNotifications from "./FacultyNotifications";
 import StudentNotifications from "./StudentNotifications";
 import UserLogin from "./UserLogin";
-// import Layout from "./Layout";
-import "./login.css";
 import AdminLogin from "./AdminLogin";
+import "./login.css";
 
 function App() {
   return (
-    <Routes>
-       <Route path="/admin-login" element={<AdminLogin />} />
-      <Route path="/user-login" element={<UserLogin />} />
-      {/* All logged-in users */}
-      {/* <Route element={<Layout />}> */}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/admin-login" element={<AdminLogin />} />
+        <Route path="/user-login" element={<UserLogin />} />
+
         <Route path="/admin-dashboard" element={<AdminDashboard />} />
         <Route path="/student-dashboard" element={<StudentDashboard />} />
         <Route path="/faculty-dashboard" element={<FacultyDashboard />} />
         <Route path="/organizer-dashboard" element={<OrganizerDashboard />} />
         <Route path="/faculty-notifications" element={<FacultyNotifications />} />
-<Route path="/student-notifications" element={<StudentNotifications />} />
-      {/* </Route> */}
-    </Routes>
+        <Route path="/student-notifications" element={<StudentNotifications />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
